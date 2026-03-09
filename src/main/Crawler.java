fc204026580955bb4a162f25122734bedcbd82b4
Jarvis Lee <jarvisgaming40@gmail.com>
2026-03-09 15:44:48 +0800

    Get last modified datetime of webpages when crawling

--- src/main/Crawler.java
+++ src/main/Crawler.java
@@ -1,5 +1,6 @@
 package comp4321.searchengine;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,6 +8,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -16,11 +18,14 @@ public class Crawler {
     public static void parse(int numPagesToCrawl) throws IOException{
         String link = "https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm";
         Optional<HTMLPage> parseResult = parseOnePage(link);
-        if (parseResult.isPresent()) System.out.println(parseOnePage(link));
+        if (parseResult.isPresent()) System.out.println(parseResult);
     }
 
     public static Optional<HTMLPage> parseOnePage(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
+        // ignoreContentType allows reading of headers, i.e. we can read Last-Modified
+        Connection.Response response = Jsoup.connect(link).ignoreContentType(true).execute();
+        Document doc = response.parse();
+
         Element body = doc.getElementsByTag("body").first();
 
         if (body == null) return Optional.empty();
@@ -28,7 +33,7 @@ public class Crawler {
         return Optional.of(new HTMLPage(
                 doc.title(),
                 link,
-                LocalDateTime.parse("2023-05-16T13:03:16"),
+                getLastModified(response),
                 body.text(),
                 getLinks(doc)
         ));
@@ -41,5 +46,12 @@ public class Crawler {
             links.add(anchor.absUrl("href"));
         }
         return links;
+    }
+
+    private static LocalDateTime getLastModified(Connection.Response response){
+        // Website responds with a format of: "Tue, 16 May 2023 05:03:16 GMT"
+        String websiteResponse = response.header("Last-Modified");
+        assert websiteResponse != null : "Last-Modified header of website is empty";
+        return LocalDateTime.parse(websiteResponse, DateTimeFormatter.RFC_1123_DATE_TIME);
     }
 }
