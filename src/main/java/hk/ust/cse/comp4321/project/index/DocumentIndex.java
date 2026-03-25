b44af2cf70eb5665f5d07b47b30354ffe95f9119
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-03-25 22:25:41 +0800

    feat: singleton now

--- src/main/java/hk/ust/cse/comp4321/project/index/DocumentIndex.java
+++ src/main/java/hk/ust/cse/comp4321/project/index/DocumentIndex.java
@@ -1,11 +1,25 @@
 package hk.ust.cse.comp4321.project.index;
 
 import hk.ust.cse.comp4321.project.database.RocksDatabaseMap;
+import org.jetbrains.annotations.NotNull;
 import org.rocksdb.RocksDBException;
+
+import java.util.Optional;
 
 
 public class DocumentIndex extends RocksDatabaseMap<String, Integer> {
-    private DocumentIndex(String databaseName) throws RocksDBException {
-        super(databaseName);
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private static Optional<DocumentIndex> INSTANCE = Optional.empty();
+
+    public static synchronized @NotNull DocumentIndex getInstance() throws RocksDBException {
+        if (INSTANCE.isPresent())
+            return INSTANCE.get();
+
+        INSTANCE = Optional.of(new DocumentIndex());
+        return INSTANCE.get();
+    }
+
+    private DocumentIndex() throws RocksDBException {
+        super("DocumentIndex");
     }
 }
--- src/main/java/hk/ust/cse/comp4321/project/index/InvertedIndex.java
+++ src/main/java/hk/ust/cse/comp4321/project/index/InvertedIndex.java
@@ -2,12 +2,25 @@ package hk.ust.cse.comp4321.project.index;
 
 import hk.ust.cse.comp4321.project.database.RocksDatabaseMap;
 import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
 import org.rocksdb.RocksDBException;
 
+import java.util.Optional;
 import java.util.TreeSet;
 
 public class InvertedIndex extends RocksDatabaseMap<String, TreeSet<Pair<Integer, Integer>>> {
-    private InvertedIndex(String databaseName) throws RocksDBException {
-        super(databaseName);
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    private static Optional<InvertedIndex> INSTANCE = Optional.empty();
+
+    public static synchronized @NotNull  InvertedIndex getInstance() throws RocksDBException {
+        if (INSTANCE.isPresent())
+            return INSTANCE.get();
+
+        INSTANCE = Optional.of(new InvertedIndex());
+        return INSTANCE.get();
+    }
+
+    private InvertedIndex() throws RocksDBException {
+        super("InvertedIndex");
     }
 }
