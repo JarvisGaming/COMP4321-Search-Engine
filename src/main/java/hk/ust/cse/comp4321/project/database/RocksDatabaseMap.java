0f3b7d20fba11799a84a33661d6663c7dee903f9
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-03-25 09:35:55 +0800

    feat: add inverted index stub

---
+++ src/main/java/hk/ust/cse/comp4321/project/database/RocksDatabaseMap.java
@@ -0,0 +1,40 @@
+package hk.ust.cse.comp4321.project.database;
+
+import org.apache.commons.lang3.SerializationException;
+import org.apache.commons.lang3.SerializationUtils;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+
+import java.io.File;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+public class RocksDatabaseMap<K extends Serializable, V extends Serializable> {
+    private final RocksDB database;
+
+    public RocksDatabaseMap(String databaseName) throws RocksDBException {
+        RocksDB.loadLibrary();
+
+        Options databaseOptions = new Options();
+        databaseOptions.setCreateIfMissing(true);
+
+        String databasePath = Optional.ofNullable(System.getenv("COMP4321_DB_DIR")).orElse("database");
+        Path path = Paths.get(databasePath, databaseName);
+        File file = path.toFile();
+        if (!file.exists() && !file.mkdirs())
+            throw new RuntimeException("failed to create directory for database: " + databasePath);
+
+        database = RocksDB.open(databaseOptions, path.toString());
+    }
+
+    public Optional<V> get(K key) throws ClassCastException, SerializationException, RocksDBException {
+        return Optional.ofNullable(database.get(SerializationUtils.serialize(key))).map(SerializationUtils::deserialize);
+    }
+
+    public void put(K key, V value) throws SerializationException, RocksDBException {
+        database.put(SerializationUtils.serialize(key), SerializationUtils.serialize(value));
+    }
+}
---
+++ src/main/java/hk/ust/cse/comp4321/project/index/InvertedIndex.java
@@ -0,0 +1,13 @@
+package hk.ust.cse.comp4321.project.index;
+
+import hk.ust.cse.comp4321.project.database.RocksDatabaseMap;
+import org.apache.commons.lang3.tuple.Pair;
+import org.rocksdb.RocksDBException;
+
+import java.util.TreeSet;
+
+public class InvertedIndex extends RocksDatabaseMap<String, TreeSet<Pair<Integer, Integer>>> {
+    private InvertedIndex(String indexDatabaseName) throws RocksDBException {
+        super(indexDatabaseName);
+    }
+}
